179a12eb24634e8fcdcbd6071ec8c60d42fed6c4
kyb0303 <kyb030393@gmail.com>
2026-06-11 19:33:37 +0900

    add pipeline code

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ spark-submit src/pipeline/preprocess_subway.py
 echo "----------Start Analysis----------"
 spark-submit src/analyze/analyze_subway.py
 echo "----------Preprocess and Analysis Finish----------"
+# automated pipeline script
