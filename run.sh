f699d3cf1f09bb9b4937cb0790a8f00a100fe04c
kyb0303 <kyb030393@gmail.com>
2026-06-11 19:28:21 +0900

    update result csv files

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "----------Start Preprocess----------"
+spark-submit src/pipeline/preprocess_subway.py
+echo "----------Start Analysis----------"
+spark-submit src/analyze/analyze_subway.py
+echo "----------Preprocess and Analysis Finish----------"
